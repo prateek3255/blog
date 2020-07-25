7345307dd5411d86e4e2f57054bba905ac6cf978
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-25 10:29:24 +0530

    :lipstick: Fixed footer image

--- _includes/shortcodes/site-footer.js
+++ _includes/shortcodes/site-footer.js
@@ -30,22 +30,22 @@ module.exports = eleventyConfig =>
         <div class="social">
         <ul>
         <li>
-            <a href="${data.author.twitter}" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="${data.author.social.twitter}" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
         </li>
                         <li>
-                            <a href="${data.author.github}" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            <a href="${data.author.social.github}" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="${data.author.stackOverflow}" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                            <a href="${data.author.social.stackOverflow}" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="${data.author.medium}" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>
+                            <a href="${data.author.social.medium}" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="${data.author.quora}" target="_blank"><i class="fa fa-quora" aria-hidden="true"></i></a>
+                            <a href="${data.author.social.quora}" target="_blank"><i class="fa fa-quora" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                                <a href="${data.author.linkedIn}" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                <a href="${data.author.social.linkedIn}" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                         </li>
                     </ul>
         </div>
