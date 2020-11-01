9cd0cfb07388865a100539e283180231284ddc62
Prateek Surana <prateeksurana3255@gmail.com>
2020-11-01 22:30:01 +0530

    :lipstick: Make site footer sticky to bottom

--- _includes/shortcodes/site-footer.js
+++ _includes/shortcodes/site-footer.js
@@ -22,32 +22,34 @@ module.exports = eleventyConfig =>
    */
   eleventyConfig.addShortcode('siteFooter', function (data) {
     return `<footer id="site_footer">
+    <div id="footer_content">
         <div>
         <p>${this.editThisPage(data)}</p>
         <p>${this.copyrightNotice(data)}</p>
         </div>
         
-        <div class="social">
-        <ul>
-        <li>
-            <a href="${data.author.social.twitter}" aria-label="Twitter" rel="noopener" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        </li>
-                        <li>
-                            <a href="${data.author.social.github}" aria-label="GitHub" rel="noopener" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="${data.author.social.stackOverflow}" aria-label="StackOverflow" rel="noopener" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="${data.author.social.medium}" aria-label="Medium" rel="noopener" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="${data.author.social.quora}" aria-label="Quora" rel="noopener" target="_blank"><i class="fa fa-quora" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                                <a href="${data.author.social.linkedIn}" aria-label="LinkedIn" rel="noopener" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        </li>
-                    </ul>
+            <div class="social">
+                <ul>
+                    <li>
+                        <a href="${data.author.social.twitter}" aria-label="Twitter" rel="noopener" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="${data.author.social.github}" aria-label="GitHub" rel="noopener" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="${data.author.social.stackOverflow}" aria-label="StackOverflow" rel="noopener" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="${data.author.social.medium}" aria-label="Medium" rel="noopener" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="${data.author.social.quora}" aria-label="Quora" rel="noopener" target="_blank"><i class="fa fa-quora" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                            <a href="${data.author.social.linkedIn}" aria-label="LinkedIn" rel="noopener" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </div>
         </div>
       </footer>`
   })
