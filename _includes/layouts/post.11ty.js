1dd2e2c9a001337dd7f13ab3c503eedd404130cc
Prateek Surana <prateeksurana3255@gmail.com>
2020-12-20 01:05:06 +0530

    :wheelchair: Accessibility fixes

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -27,7 +27,7 @@ exports.render = function (data) {
       <span>Prateek's Blog<span>
       </div>
     </a>
-    <div class="top-indicator-container" style="opacity: 0;" role="button" onClick="scrollToTop()">
+    <div class="top-indicator-container" aria-label="Scroll to top" style="opacity: 0;" role="button" onClick="scrollToTop()">
       <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="top-indicator" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
         <path d="M256 217.9L383 345c9.4 9.4 24.6 9.4 33.9 0 9.4-9.4 9.3-24.6 0-34L273 167c-9.1-9.1-23.7-9.3-33.1-.7L95 310.9c-4.7 4.7-7 10.9-7 17s2.3 12.3 7 17c9.4 9.4 24.6 9.4 33.9 0l127.1-127z"></path>
       </svg>
