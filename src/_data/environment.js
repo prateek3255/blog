91394709639822b70eaaf76e6bfa2ab4ee50245e
Prateek Surana <prateeksurana3255@gmail.com>
2021-01-26 13:31:22 +0530

    :sparkles: Added base url env

---
+++ src/_data/environment.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // The root for the site domain (without a trailing slash)
+    baseUrl: process.env.BASE_URL,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {},
+      colors: {
+        'accent-blue': '#46CBE8'
+      },
     },
   },
   variants: {},
