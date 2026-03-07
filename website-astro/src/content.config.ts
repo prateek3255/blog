041914f951d39367e2e7440c1b5d45981e53fca1
Prateek Surana <prateeksurana3255@gmail.com>
2026-03-07 19:07:48 +0530

    fix content

--- website-astro/src/content.config.ts
+++ website-astro/src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 const blog = defineCollection({
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -14,4 +14,26 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  loader: file('./src/content/projects.json'),
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    url: z.string().url(),
+    repo: z.string().url().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+const talks = defineCollection({
+  loader: file('./src/content/talks.json'),
+  schema: z.object({
+    title: z.string(),
+    event: z.string(),
+    date: z.string(),
+    description: z.string(),
+    youtubeId: z.string(),
+  }),
+});
+
+export const collections = { blog, projects, talks };
